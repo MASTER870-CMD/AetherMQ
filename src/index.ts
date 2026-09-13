9234d2d42b2f88a8d234daf4be4933736570ebbb
MASTER870-CMD <shashankgowdanb166@gmail.com>
2026-09-13 20:47:22 +0530

    fix: instantiate and start TCP server in entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("AetherMQ - High Performance Message Broker");
+﻿import { TCPServer } from './network/TCPServer';
+
+const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
+const server = new TCPServer(port);
+server.start();
